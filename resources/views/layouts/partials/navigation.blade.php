494f26c692f75caa623542d10a9aa17724a93a1e
Renato Dehnhardt <renato.dehnhardt@lojaskd.com.br>
2015-02-26 16:01:56 -0300

    Building Menu

--- resources/views/layouts/partials/navigation.blade.php
+++ resources/views/layouts/partials/navigation.blade.php
@@ -1,11 +1,19 @@
-<div class="menu">
-    <ul class="sidebar-nav">
-        <li><a href="#">Dashboard</a></li>
-        <li><a href="#">Shortcuts</a></li>
-        <li><a href="#">Overview</a></li>
-        <li><a href="#">Events</a></li>
-        <li><a href="#">About</a></li>
-        <li><a href="#">Services</a></li>
-        <li><a href="#">Contact</a></li>
-    </ul>
+<div class="sidebar-nav">
+    <div class="navbar" role="navigation">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".sidebar-navbar-collapse">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+        </div>
+        <div class="navbar-collapse collapse sidebar-navbar-collapse">
+            <ul class="nav navbar-nav">
+                <li class="active"><a href="#">Menu Item 1</a></li>
+                <li><a href="#">Menu Item 2</a></li>
+                <li><a href="#">Menu Item 4</a></li>
+            </ul>
+        </div>
+    </div>
 </div>
