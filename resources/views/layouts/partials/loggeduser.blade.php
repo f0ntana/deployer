0ea666f7858e3dee6a80235837dafe7fab04628a
Renato Dehnhardt <renato.dehnhardt@lojaskd.com.br>
2015-02-26 13:39:30 -0300

    Building a navigation!

--- resources/views/layouts/partials/loggeduser.blade.php
+++ resources/views/layouts/partials/loggeduser.blade.php
@@ -1,17 +1,8 @@
 <div class="loggeduser">
-    <div class="container-fluid">
-        <div class="row no-gutter">
-            <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
-                <figure>
-                    <img src="https://pbs.twimg.com/profile_images/2150446322/Carnaval_2011_0049_bigger.JPG" alt="Renato Dehnhardt" class="img-circle" height="73"/>
-                </figure>
-            </div>
-
-            <div class="col-xs-9 col-sm-9 col-md-9 col-lg-9">
-                <span class="name">Renato Dehnhardt</span>
-            </div>
-        </div>
-    </div>
+    <figure>
+        <img src="https://pbs.twimg.com/profile_images/2150446322/Carnaval_2011_0049_bigger.JPG" alt="Renato Dehnhardt" class="img-circle" height="73" width="73"/>
+        <figcaption>Renato Dehnhardt</figcaption>
+    </figure>
 
     <div class="actions">
         actions
