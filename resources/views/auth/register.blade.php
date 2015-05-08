52bdaaa694888b525b200e8c0333b66c12202535
Renato Dehnhardt <renato.dehnhardt@lojaskd.com.br>
2015-05-08 11:09:14 -0300

    Register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,65 +1,31 @@
-@extends('app')
+@extends('layouts.guest')
 
 @section('content')
-<div class="container-fluid">
-	<div class="row">
-		<div class="col-md-8 col-md-offset-2">
-			<div class="panel panel-default">
-				<div class="panel-heading">Register</div>
-				<div class="panel-body">
-					@if (count($errors) > 0)
-						<div class="alert alert-danger">
-							<strong>Whoops!</strong> There were some problems with your input.<br><br>
-							<ul>
-								@foreach ($errors->all() as $error)
-									<li>{{ $error }}</li>
-								@endforeach
-							</ul>
-						</div>
-					@endif
+    <h3 class="text-center">Bem-vindo! Faça seu login abaixo</h3>
 
-					<form class="form-horizontal" role="form" method="POST" action="/auth/register">
-						<input type="hidden" name="_token" value="{{ csrf_token() }}">
+    {!! Form::open() !!}
+    {!! Form::openGroup('name', 'Nome') !!}
+    {!! Form::text('name', null, ['placeholder' => 'Seu Nome']) !!}
+    {!! Form::closeGroup() !!}
 
-						<div class="form-group">
-							<label class="col-md-4 control-label">Name</label>
-							<div class="col-md-6">
-								<input type="text" class="form-control" name="name" value="{{ old('name') }}">
-							</div>
-						</div>
+    {!! Form::openGroup('email', 'Email') !!}
+    {!! Form::email('email', null, ['placeholder' => 'Seu Email']) !!}
+    {!! Form::closeGroup() !!}
 
-						<div class="form-group">
-							<label class="col-md-4 control-label">E-Mail Address</label>
-							<div class="col-md-6">
-								<input type="email" class="form-control" name="email" value="{{ old('email') }}">
-							</div>
-						</div>
+    {!! Form::openGroup('password', 'Senha') !!}
+    {!! Form::password('password', ['placeholder' => 'Senha']) !!}
+    {!! Form::closeGroup() !!}
 
-						<div class="form-group">
-							<label class="col-md-4 control-label">Password</label>
-							<div class="col-md-6">
-								<input type="password" class="form-control" name="password">
-							</div>
-						</div>
+    {!! Form::openGroup('password_confirmation', 'Confirmar Senha') !!}
+    {!! Form::password('password_confirmation', ['placeholder' => 'Repetir Senha']) !!}
+    {!! Form::closeGroup() !!}
 
-						<div class="form-group">
-							<label class="col-md-4 control-label">Confirm Password</label>
-							<div class="col-md-6">
-								<input type="password" class="form-control" name="password_confirmation">
-							</div>
-						</div>
+    {!! Form::openFormActions() !!}
+    {!! Form::submit('Registrar', ['class' => 'btn btn-primary btn-lg btn-block form-action']) !!}
+    {!! Form::closeFormActions() !!}
+    {!! Form::close() !!}
 
-						<div class="form-group">
-							<div class="col-md-6 col-md-offset-4">
-								<button type="submit" class="btn btn-primary">
-									Register
-								</button>
-							</div>
-						</div>
-					</form>
-				</div>
-			</div>
-		</div>
-	</div>
-</div>
+    <div class="login-actions">
+        <a href="/auth/login" class="btn btn-sm btn-block btn-default"><i class="fa fa-arrow-left"></i> Voltar</a>
+    </div>
 @endsection
